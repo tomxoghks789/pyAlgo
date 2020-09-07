a4a4e94dd415636d81da5ffdad71c710d8cc7310
tomxoghks789 <tomxoghks789@naver.com>
2020-09-07 18:06:15 +0900

    test

--- Apart.py
+++ Apart.py
@@ -1,3 +1,12 @@
-print("Hello!")
-print(1+1)
-print("qqq")
+import sys
+
+N = int(input())
+
+mapData = [[0] * N for _ in range(N)]
+
+for i in range(N):
+    temp = sys.stdin.readline()
+    for j in range(N):
+        mapData[i][j] = int(temp[j])
+
+visited = [[0 for _ in range(N)] for _ in range(N)]
