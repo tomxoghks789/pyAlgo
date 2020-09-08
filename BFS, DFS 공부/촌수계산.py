bcb951cdfcf33b6974c41613ba7cdf2e94a2359b
tomxoghks789 <tomxoghks789@naver.com>
2020-09-08 17:23:22 +0900

    Study

--- BFS, DFS 공부/촌수계산.py
+++ BFS, DFS 공부/촌수계산.py
@@ -1,3 +1,38 @@
-N = int(input())
+from collections import deque
 
-print(N)
+N = int(input())
+start, end = map(int, input().split())
+M = int(input())
+graph = [set([]) for i in range(N + 1)]
+
+def bfs(graph, start, end):
+    visited = []
+    que = deque([start])
+    qc = deque([0])
+    counter = 0
+    while que:
+        node = que.popleft()
+        c = qc.popleft()
+        if node in visited:
+            continue
+        if node == end:
+            counter = c
+            break
+        if node not in visited:
+            visited.append(node)
+            que.extend(sorted(graph[node]))
+            for _ in range(len(graph[node])):
+                qc.append(c + 1)
+
+    if counter == 0:
+        counter = -1
+
+    return counter
+
+
+for i in range(M):
+    V1, V2 = map(int, input().split())
+    graph[V1].add(V2)
+    graph[V2].add(V1)
+
+print(bfs(graph, start, end))
