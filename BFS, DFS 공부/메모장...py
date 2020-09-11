b60206060e602f7f4621d5640dac431fe9a87adc
tomxoghks789 <tomxoghks789@naver.com>
2020-09-11 22:02:26 +0900

    Study 11/09/20

--- BFS, DFS 공부/메모장...py
+++ BFS, DFS 공부/메모장...py
@@ -1,15 +1,13 @@
-N = int(input())
-txt = input()
-A = 0
-B = 0
-for i in range(len(txt)):
-    if txt[i] == "A":
-        A +=1
-    else:
-        B +=1
-if A > B :
-    print("A")
-elif B > A:
-    print("B")
-else:
-    print("Tie")
+import sys
+x, y = map(int, input().split())
+direction = [[-1, 0], [1, 0], [0, 1], [0, -1]]
+visited = [[0] * x for _ in range(y)]
+mapData = [[0] * x for _ in range(y)]
+counter = 0
+
+for i in range(y):
+    temp = sys.stdin.readline().split()
+    for j in range(x):
+        mapData[i][j] = int(temp[j])
+
+print(mapData)
