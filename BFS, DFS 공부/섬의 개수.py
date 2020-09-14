a5ab811760fe9c6bad63de22ff3f893629c81396
tomxoghks789 <tomxoghks789@naver.com>
2020-09-14 17:39:36 +0900

    Study 14/09/20

--- BFS, DFS 공부/섬의 개수.py
+++ BFS, DFS 공부/섬의 개수.py
@@ -7,30 +7,36 @@ direction = [[-1, 0], [1, 0], [0, 1], [0, -1], [-1, -1], [1, 1], [-1, 1], [1, -1
 def bfs(x, y):
     que = Queue()
     que.put([x, y])
+    visited[x][y] = 1
+
     while not que.empty():
         tx, ty = que.get()
+
         for i in range(len(direction)):
             nx = tx + direction[i][0]
             ny = ty + direction[i][1]
-            if 0 <= nx < y and 0 <= ny < x:
+            if 0 <= nx < h and 0 <= ny < w:
                 if mapData[nx][ny] and not visited[nx][ny]:
                     que.put([nx, ny])
                     visited[nx][ny] = 1
 
 while True:
     counter =0
-    x, y = map(int, input().split())
-    if not x and not y:
+    w, h = map(int, input().split())
+    if not h and not w:
         break
-    mapData = [[0] * x for _ in range(y)]
-    visited = [[0] * x for _ in range(y)]
-    for i in range(y):
+    mapData = [[0] * w for _ in range(h)]
+    visited = [[0] * w for _ in range(h)]
+
+    for i in range(h):
         temp = sys.stdin.readline().split()
-        for j in range(x):
+        for j in range(w):
             mapData[i][j] = int(temp[j])
-    for i in range(y):
-        for j in range(x):
+
+    for i in range(h):
+        for j in range(w):
             if mapData[i][j] and not visited[i][j]:
                 bfs(i, j)
                 counter += 1
+
     print(counter)
