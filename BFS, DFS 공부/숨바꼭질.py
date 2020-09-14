cb87c2f155ab101ab14145a63de182637d3c1fcd
tomxoghks789 <tomxoghks789@naver.com>
2020-09-15 01:31:40 +0900

    Study 14/09/20

--- BFS, DFS 공부/숨바꼭질.py
+++ BFS, DFS 공부/숨바꼭질.py
@@ -1,32 +1,28 @@
 # 1697
 from collections import deque
+
+LIMIT = 100001
+
 N, K = map(int, input().split())
-visited = []
 
 def find():
     counter = 0
     que = deque([N])
     qc = deque([0])
-    visited.append(N)
 
     while que:
         temp = que.popleft()
-        c = qc.popleft()
-        counter = c
+        counter = qc.popleft()
 
         if temp == K:
             break
 
         for nx in (temp+1, temp-1, temp*2):
-            if N <= nx < K:
-                if nx not in visited:
-                    que.append(nx)
-                    visited.append(nx)
-                    qc.append(c + 1)
-
+            if 0 <= nx < LIMIT and not mapData[nx]:
+                que.append(nx)
+                qc.append(counter + 1)
+                mapData[nx] = 1
     return counter
 
-mapData = list(range(N, K+1))
+mapData = [0] * LIMIT
 print(find())
-
-print
