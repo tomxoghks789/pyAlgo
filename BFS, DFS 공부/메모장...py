3329a6f1ee07ab4f62353d133e39c0ec2a5171fc
tomxoghks789 <tomxoghks789@naver.com>
2020-09-14 23:28:48 +0900

    Study 14/09/20

--- BFS, DFS 공부/메모장...py
+++
@@ -1,8 +0,0 @@
-from collections import deque
-
-temp = deque()
-temp.append([3, 4])
-temp.append([6, 7])
-print(temp)
-x, y = temp.popleft()
-print(x, y)
--- BFS, DFS 공부/숨바꼭질.py
+++ BFS, DFS 공부/숨바꼭질.py
@@ -1,3 +1,44 @@
 # 1697
-
+from collections import deque
 N, K = map(int, input().split())
+visited = []
+
+def find():
+    counter = 0
+    que = deque([N])
+    qc = deque([0])
+    visited.append(N)
+
+    while que:
+        temp = que.popleft()
+        c = qc.popleft()
+        counter = c
+
+        if temp == K:
+            break
+
+        nx = temp + 1
+        if N <= nx < K:
+            if nx not in visited:
+                que.append(nx)
+                visited.append(nx)
+                qc.append(c + 1)
+
+        nx = temp - 1
+        if N <= nx < K:
+            if nx not in visited:
+                que.append(nx)
+                visited.append(nx)
+                qc.append(c + 1)
+
+        nx = temp * 2
+        if N <= nx < K:
+            if nx not in visited:
+                que.append(nx)
+                visited.append(nx)
+                qc.append(c + 1)
+
+    return counter
+
+mapData = list(range(N, K+1))
+print(find())
