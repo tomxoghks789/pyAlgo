8caf372f80aaa0cba0818957e06767d5e974fa40
tomxoghks789 <tomxoghks789@naver.com>
2020-09-21 18:11:29 +0900

    Study 21/09/20

--- BFS, DFS 공부/말이 되고픈 원숭이.py
+++ BFS, DFS 공부/말이 되고픈 원숭이.py
@@ -1,43 +1,48 @@
 import sys
 from queue import Queue
 
-def BFS():
+K =int(input())
+W, H = map(int, input().split())
+data = []
+visited = [[0] * W for _ in range(H)]
+
+def BFS(K):
+    flag = 0
     counter = 0
     que = Queue()
     que.put([0, 0, 0])
-    visited[0][0] = True
+    visited[0][0] = 1
     while not que.empty():
         tx, ty, tc = que.get()
-        if visited[tx][ty]:
-            continue
         counter = tc
-        if K != 0:
+        if tx == H - 1 and ty == W - 1:
+            flag = 1
+            break
+        if K > 0:
             for i in range(len(kDirection)):
                 nx = tx + kDirection[i][0]
                 ny = ty + kDirection[i][1]
                 if 0<= nx < H and 0 <= ny < W:
                     if not data[nx][ny] and not visited[nx][ny]:
+                        # 여기에 가는길에 뭐 없나 체크
                         que.put([nx, ny, tc +1])
-                        visited[nx][ny]
+                        visited[nx][ny] = 1
                         K = K - 1
         else:
             for i in range(len(direction)):
                 nx = tx + direction[i][0]
                 ny = ty + direction[i][1]
                 if 0<= nx < H and 0 <= ny < W:
-                    if data[nx][ny] and not visited[nx][ny]:
+                    if not data[nx][ny] and not visited[nx][ny]:
                         que.put([nx, ny, tc +1])
                         visited[nx][ny] = 1
+    if flag == 0:
+        return -1
     return counter
 
 direction = [[-1, 0], [1, 0], [0, 1], [0, -1]]
 kDirection = [[-1, -2], [-2, -1], [-2, 1], [-1, 2], [1, 2], [2, 1], [2, -1], [1, -2]]
 
-K = input()
-W, H = map(int, input().split())
-data = []
-visited = [[False] * W for _ in range(H)]
-print(visited)
 for _ in range(H):
     data.append(list(map(int, sys.stdin.readline().split())))
-print(BFS())
+print(BFS(K))
