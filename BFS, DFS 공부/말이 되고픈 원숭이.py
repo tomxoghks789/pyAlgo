7480794de54454b589f50a18980bde0a1dc7cbfe
tomxoghks789 <tomxoghks789@naver.com>
2020-09-23 18:01:59 +0900

    Study 22/09/20

--- BFS, DFS 공부/말이 되고픈 원숭이.py
+++ BFS, DFS 공부/말이 되고픈 원숭이.py
@@ -1,40 +1,37 @@
 import sys
-from queue import Queue
+from collections import deque
 
 K = int(input())
 W, H = map(int, input().split())
 data = []
-visited = [[0] * W for _ in range(H)]
+visited = [[[0 for i in range(K + 1)] for i in range(W)] for i in range(H)]
 
 def BFS():
     counter = 0
-    que = Queue()
-    que.put([0, 0, 0, K])
-    visited[0][0] = 1
+    que = deque()
+    que.append((0, 0, K))
+    visited[0][0][0] = 1
 
-    while not que.empty():
-        tx, ty, tc, tk = que.get()
-        counter = tc
+    while que:
+        tx, ty, tk = que.popleft()
 
         if tx == H - 1 and ty == W - 1:
-            return counter
+            return visited[tx][ty][tk]
 
         if tk > 0:
             for i in range(len(kDirection)):
                 nx = tx + kDirection[i][0]
                 ny = ty + kDirection[i][1]
-                if 0 <= nx < H and 0 <= ny < W:
-                    if not data[nx][ny] and not visited[nx][ny]:
-                        que.put([nx, ny, tc +1, tk - 1])
-                        visited[nx][ny] = 1
+                if 0 <= nx < H and 0 <= ny < W and not data[nx][ny] and not visited[nx][ny][tk - 1]:
+                        que.append((nx, ny, tk - 1))
+                        visited[nx][ny][tk - 1] = visited[tx][ty][tk] + 1
 
         for i in range(len(direction)):
             nx = tx + direction[i][0]
             ny = ty + direction[i][1]
-            if 0 <= nx < H and 0 <= ny < W:
-                if not data[nx][ny] and not visited[nx][ny]:
-                    que.put([nx, ny, tc + 1, tk])
-                    visited[nx][ny] = 1
+            if 0 <= nx < H and 0 <= ny < W and not data[nx][ny] and not visited[nx][ny][tk]:
+                    que.append((nx, ny, tk))
+                    visited[nx][ny][tk] = visited[tx][ty][tk] + 1
     return -1
 
 direction = [[-1, 0], [1, 0], [0, 1], [0, -1]]
