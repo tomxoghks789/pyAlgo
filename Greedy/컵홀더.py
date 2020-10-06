1d97e3246085b8d3bdc0b9a893aaed121baba359
tomxoghks789 <tomxoghks789@naver.com>
2020-10-06 18:15:46 +0900

    06/10/20 Study

--- Greedy/컵홀더.py
+++ Greedy/컵홀더.py
@@ -1,1 +1,10 @@
 #2810번
+N = int(input())
+S = input()
+counter = 0
+if S.count("S") == N:
+    print(S.count("S"))
+else:
+    counter += S.count("LL")
+    counter += S.count("S")
+    print(counter + 1)
