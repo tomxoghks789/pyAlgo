ff15c688479e47395431bb1b5453b209314d9346
tomxoghks789 <tomxoghks789@naver.com>
2020-10-07 23:10:07 +0900

    07/10/20 Study

---
+++ Greedy/동전 0.py
@@ -0,0 +1,19 @@
+# 11047번  동전 0
+
+N, K = map(int, input().split())
+counter = 0
+A = []
+
+for _ in range(N):
+    temp = int(input())
+    if temp <= K:
+        A.append(temp)
+
+A.reverse()
+
+for i in A:
+    x, y = divmod(K, i)
+    K -= x * i
+    counter += x
+
+print(counter)
