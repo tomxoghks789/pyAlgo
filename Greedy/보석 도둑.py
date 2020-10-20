15fe43f4a29c32cd9c4b6604bb32d91c783837ab
tomxoghks789 <tomxoghks789@naver.com>
2020-10-20 14:31:41 +0900

    20/10/20 Study

---
+++ Greedy/보석 도둑.py
@@ -0,0 +1,28 @@
+import sys
+import heapq
+
+def greedy(jewel, bagData):
+    ans = 0
+    pickedJewel = []
+    for _ in range(K):
+        bagSize = heapq.heappop(bagData)
+        while jewel and bagSize >= jewel[0][0]:
+            w, v = heapq.heappop(jewel)
+            heapq.heappush(pickedJewel, -v)
+        if pickedJewel:
+            ans -= heapq.heappop(pickedJewel)
+        elif not jewel:
+            break
+    return ans
+
+N, K = map(int, sys.stdin.readline().split())
+M= []
+Bag= []
+
+for _ in range(N):
+    heapq.heappush(M, list(map(int, sys.stdin.readline().split())))
+
+for _ in range(K):
+    heapq.heappush(Bag, int(sys.stdin.readline()))
+
+print(greedy(M, Bag))
