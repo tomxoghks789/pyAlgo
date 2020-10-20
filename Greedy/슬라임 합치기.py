8634a64e75ddcd96aa19d5f7fda02cf465eda2ed
tomxoghks789 <tomxoghks789@naver.com>
2020-10-20 18:32:21 +0900

    20/10/20 Study

--- Greedy/슬라임 합치기.py
+++ Greedy/슬라임 합치기.py
@@ -1,4 +1,17 @@
-N = int(input())
+# 14241
+import sys
 
-w, h = map(int, input().split())
+def greedy(slime, N):
+    ans = 0
+    for i in range(N - 1):
+        x = slime[i]
+        y = slime[i + 1]
+        ans += x * y
+        slime[i + 1] = x + y
+    return ans
 
+N = int(sys.stdin.readline())
+
+slime = list(map(int, sys.stdin.readline().split()))
+
+print(greedy(slime, N))
