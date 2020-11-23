12f23568140be75b1462cfb128ac0911cafc1fde
TaeHwan KIM <tomxoghks789@naver.com>
2020-11-23 23:56:47 +0900

    20/11/13 Study

--- Implementation/그룹 단어 체커.py
+++ Implementation/그룹 단어 체커.py
@@ -1,15 +1,16 @@
 # 1316번
-N = int(input())
+import sys
+N = int(sys.stdin.readline())
 ans = 0
 for i in range(N):
     flag = 1
-    inputData = input()
+    inputData = sys.stdin.readline()
     temp = [inputData[0]]
     for j in range(1, len(inputData)):
         if inputData[j] not in temp:
             temp.append(inputData[j])
         else:
-            if inputData[j] != temp[len(temp) - 1]:
+            if inputData[j] != temp[- 1]:
                 flag = 0
     if flag:
         ans += 1
