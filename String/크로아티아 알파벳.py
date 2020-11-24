15b150ed15b0ac77d2891b9bc3b1cfc752f9541d
TaeHwan KIM <tomxoghks789@naver.com>
2020-11-25 02:34:29 +0900

    20/11/25 Study

--- String/크로아티아 알파벳.py
+++ String/크로아티아 알파벳.py
@@ -1,12 +1,8 @@
 # 2941
 def converter(str):
     dataList = ["c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="]
-    for i in range(len(str)):
-        if i >= len(str):
-            break
-        for j in range(8):
-            if str[i] in dataList[j]:
-                str = str.replace(dataList[j], "0", 1)
+    for i in range(8):
+        str = str.replace(dataList[i], "0")
     return len(str)
 
 print(converter(input()))
