a96dcaa877e07560d937d4fb0bb56aa16e11a4ed
TaeHwan KIM <tomxoghks789@naver.com>
2020-11-30 03:59:03 +0900

    20/11/30 Study

--- Sort/수 정렬하기 3.py
+++ Sort/수 정렬하기 3.py
@@ -1,12 +1,12 @@
 # 10989
-import sys
+f = open(0)
+f.readline()
 
 LIMIT = 10001
 data = [0] * LIMIT
-dataLen = int(sys.stdin.readline())
 
-for i in range(dataLen):
-    data[int(sys.stdin.readline())] += 1
+for i in f:
+    data[int(i)] += 1
 for i in range(LIMIT):
     dataVal = data[i]
     if dataVal != 0:
