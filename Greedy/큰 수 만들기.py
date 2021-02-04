e530f490e5cdd3167229aa28eba317d6b5903d36
TaeHwan KIM <tomxoghks789@naver.com>
2021-02-04 22:44:53 +0900

    21/02/04 Study

--- Greedy/큰 수 만들기.py
+++ Greedy/큰 수 만들기.py
@@ -1,7 +1,20 @@
 # 프로그래머스 큰 수 만들기
 
 def solution(number, k):
-    answer = []
-    return max(answer)
+    ans = [number[0]]
+    for i in range(1, len(number)):
+        if ans[-1] > number[i]:
+            ans.append(number[i])
+        else:
+            while True:
+                if k != 0 and len(ans) != 0 and number[i] > ans[-1]:
+                    ans.pop(-1)
+                    k -= 1
+                else:
+                    ans.append(number[i])
+                    break
+    if k != 0:
+        return "".join(ans)[:-k]
+    return "".join(ans)
 
-print(solution("1924", 2))
+print(solution("4177252841", 4))
