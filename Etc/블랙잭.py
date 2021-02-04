c7aed39dcac5d3feeb510087783124415cc37ea3
TaeHwan KIM <tomxoghks789@naver.com>
2021-02-04 18:10:04 +0900

    21/02/04 Study

---
+++ Etc/블랙잭.py
@@ -0,0 +1,16 @@
+# 2798
+
+N, M = map(int, input().split())
+ans = []
+dataList = list(map(int, input().split()))
+for i in range(len(dataList)):
+    for j in range(i + 1, len(dataList)):
+        for k in range(j + 1, len(dataList)):
+            temp = dataList[i] + dataList[j] + dataList[k]
+            if temp <= M:
+                ans.append(temp)
+if M in ans:
+    print(M)
+else:
+    ans.sort()
+    print(ans[-1])
