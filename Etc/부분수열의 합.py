ec67888991504c01c6bd3b93e2e289cf5479687e
TaeHwan KIM <tomxoghks789@naver.com>
2021-02-04 22:05:23 +0900

    21/02/04 Study

---
+++ Etc/부분수열의 합.py
@@ -0,0 +1,17 @@
+# 1182
+
+from itertools import combinations
+
+N, S = map(int, input().split())
+
+dataList = list(map(int, input().split()))
+ans = 0
+
+for i in range(1, len(dataList) + 1):
+    tempList = list(combinations(dataList, i))
+    for j in range(len(tempList)):
+        sumVal = sum(tempList[j])
+        if sumVal == S:
+            ans += 1
+
+print(ans)
