58c9d05c53ae6ced97082f8561c0df8520c6ddd8
TaeHwan KIM <tomxoghks789@naver.com>
2021-02-08 01:03:45 +0900

    21/02/08 Study

---
+++ BFS/트리의 부모찾기.py
@@ -0,0 +1,30 @@
+# 11725
+
+from collections import deque
+import sys
+N = int(input())
+
+ans = [0] * (N + 1)
+mapData = [[] for _ in range(N + 1)]
+
+for i in range(N - 1):
+    V1, V2 = map(int, sys.stdin.readline().split())
+    mapData[V1].append(V2)
+    mapData[V2].append(V1)
+
+que = deque([1])
+visited = [0] * (N + 1)
+print(mapData)
+while que:
+    now = que.popleft()
+    for i in mapData[now]:
+        print(i)
+        if not visited[i]:
+            ans[i] = now
+            que.append(i)
+            visited[i] = 1
+    print(ans)
+    print()
+for i in ans[2:]:
+    print(i)
+
